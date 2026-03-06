da2b00124c4124bcb5d3fa7500da58fa0f8640b5
Mehdi <mehdi.ert04@gmail.com>
2026-03-06 23:42:12 +0000

    M -

--- Web/Backend/database/migrations/2026_02_18_180050_create_receveur_profiles_table.php
+++ Web/Backend/database/migrations/2026_02_18_180050_create_receveur_profiles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('receveur_profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('nom');
             $table->string('prenom');
             $table->string('telephone')->unique();
